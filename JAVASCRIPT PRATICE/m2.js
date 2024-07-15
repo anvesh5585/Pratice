af5d9abeb8cd6d4eaa96369d3c2a1486cb7d89d9
anvesh5585 <167979314+anvesh5585@users.noreply.github.com>
2024-07-15 10:58:28 +0530

    Add files via upload

---
+++ JAVASCRIPT PRATICE/m2.js
@@ -0,0 +1,3 @@
+import {fun1 as name, fun2 as number} from "./m22.js"
+name("G Anvesh");
+number(2408);
---
+++ JAVASCRIPT PRATICE/m22.js
@@ -0,0 +1,7 @@
+function fun1(name){
+    console.log("My name is "+name);
+}
+function fun2(number){
+    console.log("My number is "+number);
+}
+export {fun1,fun2};
